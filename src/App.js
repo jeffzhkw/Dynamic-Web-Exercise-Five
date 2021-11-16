f3b8f49a4c5bdf50dcbaf60c01df810f133b0eeb
Kewei Zhang <jeffzhkw@gmail.com>
2021-11-16 08:59:04 -0500

    add react router, minor styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+//Pages import
+import Login from "./containers/Login";
+import CreateUser from "./containers/CreateUser";
+import UserProfile from "./containers/UserProfile";
+import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
+      <Header /> {/* state might impact, not data */}
       <p>Hello from app</p>
+      <Router>
+        <Routes>
+          <Route path="/user/:id" element={<UserProfile />} />
+          <Route path="/create" element={<CreateUser />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/containers/UserProfile.js
+++ src/containers/UserProfile.js
@@ -4,4 +4,4 @@ function UserProfile() {
   return <div></div>;
 }
 
-export default userProfile;
+export default UserProfile;
