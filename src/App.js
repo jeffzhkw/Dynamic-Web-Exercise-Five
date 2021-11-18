b41e824df4e3163555b984b19e2ac898b6210065
Kewei Zhang <jeffzhkw@gmail.com>
2021-11-18 09:39:13 -0500

    add firebase auth

--- src/App.js
+++ src/App.js
@@ -1,19 +1,69 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+//Firebase
+import { initializeApp } from "firebase/app"; // Import the functions you need from the SDKs you need
+// Your web app's Firebase configuration
+import firebaseConfig from "./components/FirebaseConfig";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 //Pages import
 import Login from "./containers/Login";
 import CreateUser from "./containers/CreateUser";
 import UserProfile from "./containers/UserProfile";
 import Header from "./components/Header";
+
 function App() {
+  const [appInitialized, setAppInitialized] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false); // Track if user is logged in
+  const [loading, setLoading] = useState(true); // Check to see if there is any loading ... prevent multiple time submit
+  const [userInformation, setUserInformation] = useState({}); //store userInfo in state.
+
+  useEffect(() => {
+    initializeApp(firebaseConfig); // Initialize Firebase
+    setAppInitialized(true);
+  }, []);
+
+  useEffect(() => {
+    if (appInitialized) {
+      //check to see if User is logged in
+      //user leads page, check their status
+      //set state accordingly
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          // User is signed in, see docs for a list of available properties
+          // https://firebase.google.com/docs/reference/js/firebase.User
+          const uid = user.uid;
+          setUserInformation(user);
+          setLoggedIn(true);
+          // ...
+        } else {
+          // User is signed out
+          setUserInformation({});
+          setLoggedIn(false);
+        }
+      });
+      setLoading(false);
+    }
+  }, [appInitialized]);
+
+  if (loading) return null;
+
   return (
     <div className="App">
       <Header /> {/* state might impact, not data */}
-      <p>Hello from app</p>
       <Router>
         <Routes>
           <Route path="/user/:id" element={<UserProfile />} />
-          <Route path="/create" element={<CreateUser />} />
+          <Route
+            path="/create"
+            element={
+              <CreateUser
+                setLoggedIn={setLoggedIn}
+                setUserInformation={setUserInformation}
+              />
+            }
+          />
           <Route path="/" element={<Login />} />
         </Routes>
       </Router>
--- src/containers/CreateUser.js
+++ src/containers/CreateUser.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useCallback } from "react";
 import CreateUserForm from "../components/CreateUserForm";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-function CreateUser() {
+//page of data source
+function CreateUser({ setLoggedIn, setUserInformation }) {
+  const signUpUser = useCallback((e) => {
+    e.preventDefault(); //let us have more control over our form
+
+    //currentTarget.email: targeting the input element
+    const email = e.currentTarget.email.value;
+    const password = e.currentTarget.password.value;
+
+    console.log({ email, password });
+
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        setLoggedIn(true);
+        setUserInformation({
+          email: user.email,
+          displayNmae: user.displayName,
+          uid: user.uid,
+          accessToken: user.accessToken,
+        });
+        console.log({ user });
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.warn({ errorCode, errorMessage });
+        // ..
+      });
+  }, []);
+
   return (
     <div>
       <h1>Create User</h1>
-      <CreateUserForm />
+      <CreateUserForm signUpUser={signUpUser} />
     </div>
   );
 }
